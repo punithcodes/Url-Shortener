ec608fdeee5174f50f824f4d08136dd9a96e9162
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-09-30 20:38:36 +0530

    Update test_setup.py

--- project/app/tests/test_setup.py
+++ project/app/tests/test_setup.py
@@ -1,9 +1,10 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
-
+# This class is responsible for creating test class by inheriting APITestCase 
 class TestSetUp(APITestCase):
-
+    
+    # This method is resposible to set all the variables before executing any TestCases. This SetUp method gets called before execution of each TestCase.
     def setUp(self):
         self.get_urls = reverse('all_urls')
         self.create_urls = reverse('create_link')
@@ -14,6 +15,6 @@ class TestSetUp(APITestCase):
             'invalid_url': "www.google.com"
         }
         return super().setUp()
-
+    
     def tearDown(self):
         return super().tearDown()

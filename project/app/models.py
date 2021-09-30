69b70dd4b8609937b30c612634fbb255ca1805c7
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-09-30 20:33:05 +0530

    Update models.py

--- project/app/models.py
+++ project/app/models.py
@@ -3,11 +3,12 @@ from random import choices
 from string import ascii_letters
 from django.conf import settings
 
-
+# This class is responsible for the creation of Url table in the database 
 class Url(models.Model):
     original_url = models.URLField()
     short_url = models.URLField(blank=True, null=True)
-
+    
+    # This function is responsible for creating short url which is not repeated in database.
     def shortener(self):
         while True:
             random_string = ''.join(choices(ascii_letters, k=5))
@@ -16,7 +17,8 @@ class Url(models.Model):
             if not Url.objects.filter(short_url=new_link).exists():
                 break
         return new_link
-
+    
+    # Here I have overridden the save method and assigning the created new url to the class variable "short_url" and saving it in database.
     def save(self, *args, **kwargs):
         if not self.short_url:
             new_link = self.shortener()

d3d14dc11204e7e3b066565f8c1f2e33e77e85f3
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-09-30 20:27:42 +0530

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+#app urls and swagger urls are added in these url patterns
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
